2fce19efe4d8a774e6cca8de89ac03646cf3699b
dario platania <dariopl91@gmail.com>
2019-05-10 17:08:18 +0200

    insert salva() method into form-prodotto

--- my-amazon-admin/src/app/components/form-prodotto/form-prodotto.component.ts
+++ my-amazon-admin/src/app/components/form-prodotto/form-prodotto.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Prodotto } from 'src/app/model/prodotto';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-form-prodotto',
@@ -10,11 +13,19 @@ export class FormProdottoComponent implements OnInit {
 
   prodotto : Prodotto;
 
-  constructor() {
+  constructor(
+    private httpClient : HttpClient,
+    private router : Router
+  ) {
     this.prodotto = new Prodotto();
   }
 
   ngOnInit() {
+  }
+
+  salva(){
+    this.httpClient.post('http://localhost:3000/prodotti',this.prodotto)
+    .subscribe(()=>this.router.navigateByUrl('/elenco-prodotti'))
   }
 
 }
